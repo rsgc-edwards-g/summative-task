dc0a0620470565673433d006248bb156e204f391
rsgc-edwards-g <gedwards@rsgc.on.ca>
2016-05-23 18:22:27 -0400

    Continued Developing Classes
    Created the Star class necessary to finish my project. There was a
    problem with my CVector class, because Processing is saying it doesn’t
    exist, that I haven’t been able to fix.

---
+++ Project/Star.java
@@ -0,0 +1,8 @@
+class Star extends Celestial {
+  
+  Star(float _mass, float _radius, float locationX, float locationY, int fuel){
+    super(_mass, _radius, locationX, locationY);
+    
+  }
+  
+}
